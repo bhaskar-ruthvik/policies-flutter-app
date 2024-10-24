1b8a4b793362fcdfca24bff435b2ee891469d2ae
Pranathi Voora <pranathivoora2@gmail.com>
2024-10-24 11:38:15 +0530

    transcription and speech to text voice ip completed properly

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:policies_app/screens/flowchart_screen.dart';
 import 'package:policies_app/screens/paragraph_screen.dart';
 import 'package:policies_app/screens/yes_no_screen.dart';
 import 'package:http/http.dart' as http;
-import 'package:policies_app/widgets/input_form2.dart';
+import 'package:policies_app/widgets/input_form3.dart';
 
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import path_provider_foundation
 import record_darwin
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
